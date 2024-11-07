04178bf0bbf202cc0f84d0a1f1e47c0562c45ad5
Your Name <you@example.com>
2024-11-07 14:14:25 -0300

    Primeiro commit

---
+++ src/main/java/com/example/magda/MagdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.magda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/magda/MagdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
